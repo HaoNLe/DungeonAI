258e544dc903a85631bfce0b6855afe30f47552b
haonle <HaoLe@berkeley.edu>
2018-10-27 13:38:59 -0700

    player movement and sprite flipping done

---
+++ DungeonAI/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    private Animator animator;
+    private CharacterController characterController;
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        
+        // Play run animation and move the character
+        animator.SetFloat("playerRun", Mathf.Abs(moveVertical) + Mathf.Abs(moveHorizontal));
+        float dy = moveVertical * speed * Time.deltaTime;
+        float dx = moveHorizontal * speed * Time.deltaTime;
+        transform.position = new Vector2(transform.position.x + dx, transform.position.y + dy);
+
+        // Flip sprite if moving to left
+        if (moveHorizontal < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (moveHorizontal > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+
+    }
+}
